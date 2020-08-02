d9ee54081af8d72bb211f9b86a99daa125880fc5
Eric Larson <ericlarsonbroom@gmail.com>
2020-08-01 19:17:17 -0600

    added service for location images with corresponding test

---
+++ app/services/image_service.rb
@@ -0,0 +1,18 @@
+class ImageService
+
+  def find_image(location)
+    response = conn.get('search/photos') do |req|
+      req.params['query'] = location
+    end
+    JSON.parse(response.body, symbolize_names: true)
+  end
+
+  private
+
+  def conn
+    Faraday.new("https://api.unsplash.com") do |faraday|
+      faraday.params['client_id'] = ENV['UNSPLASH_API_KEY']
+    end
+  end
+
+end
