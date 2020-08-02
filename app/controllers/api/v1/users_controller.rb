ab093651ef8b5df0077007dbe01d566b6c8a5ffa
Eric Larson <ericlarsonbroom@gmail.com>
2020-08-02 16:56:51 -0600

    added bcrypt confirmation to user model

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,13 +1,14 @@
 class Api::V1::UsersController < ApplicationController
 
   def create
-
+    
+    User.create(user_params)
   end
 
   private
 
   def user_params
-
+    params.permit(:email, :password).merge()
   end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  validates_presence_of :email, :password
+  validates :email, uniqueness: true
+  validates_confirmation_of :password, on: :create
+
+    has_secure_password
+
 
 
 end
