44df5ab2f511f4cdce05e4d6b7e740ff175bd81a
Eric Larson <ericlarsonbroom@gmail.com>
2020-08-03 09:52:26 -0600

    Created test for trial endpoint

---
+++ spec/requests/api/v1/trails_spec.rb
@@ -0,0 +1,24 @@
+require 'rails_helper'
+
+describe 'trails endpoint' do
+  it 'get information for trials in area' do
+
+    get '/api/v1/trails', params: {location: 'denver,co'}
+
+    expect(response).to be_successful
+
+    trails = JSON.parse(response.body, symbolize_names: true)
+
+    expect(trails[:data][:attributes][:location]).to eq('denver,co')
+
+    expect(trails[:data][:attributes][:forecast]).to have_key(:summary)
+    expect(trails[:data][:attributes][:forecast]).to have_key(:temperature)
+
+    expect(trails[:data][:attributes][:trails].first).to have_key(:name)
+    expect(trails[:data][:attributes][:trails].first).to have_key(:summary)
+    expect(trails[:data][:attributes][:trails].first).to have_key(:difficulty)
+    expect(trails[:data][:attributes][:trails].first).to have_key(:location)
+    expect(trails[:data][:attributes][:trails].first).to have_key(:distance_to_trail)
+
+  end
+end
