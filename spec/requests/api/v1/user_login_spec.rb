308f289134271f4926726c12317d0cef1aa59dbf
Eric Larson <ericlarsonbroom@gmail.com>
2020-08-03 17:11:37 -0600

    added login endpoint

--- spec/requests/api/v1/user_login_spec.rb
+++ spec/requests/api/v1/user_login_spec.rb
@@ -2,18 +2,24 @@ require 'rails_helper'
 
 describe "user login" do
   it "the user can login" do
-  body = {
+    user = User.create({
+      email: "whatever@example.com",
+      password: "password",
+      api_key: "1234"
+      })
+
+  login_body = {
   "email": "whatever@example.com",
   "password": "password"
   }
 
   headers = { "CONTENT_TYPE" => "application/json" }
 
-    post '/api/v1/sessions', params: body.to_json, :headers => headers
+    post '/api/v1/sessions', params: login_body.to_json, :headers => headers
 
-    expect(response).to be_successful
 
-    response = JSON.parse(response.body, symbolize_names: true)
+
+    response = JSON.parse(body, symbolize_names: true)
     expect(response[:data][:attributes]).to have_key(:email)
     expect(response[:data][:attributes]).to have_key(:api_key)
 
--- spec/requests/api/v1/user_registration_spec.rb
+++ spec/requests/api/v1/user_registration_spec.rb
@@ -13,7 +13,7 @@ describe "User registration" do
 
     expect(response).to be_successful
     registration_response = JSON.parse(response.body, symbolize_names:true)
-    binding.pry
+
     expect(response.status).to eq(201)
     expect(registration_response[:data][:attributes]).to have_key(:email)
     expect(registration_response[:data][:attributes]).to have_key(:api_key)
