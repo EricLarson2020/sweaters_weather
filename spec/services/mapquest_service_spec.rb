a6cf255e0ce6843279bf452f14724fb499f27a8c
Eric Larson <ericlarsonbroom@gmail.com>
2020-08-04 09:57:43 -0600

    Created test for map quest service travel time

--- spec/services/mapquest_service_spec.rb
+++ spec/services/mapquest_service_spec.rb
@@ -7,7 +7,19 @@ describe "map quest service" do
 
 
     expect(coords).to be_a Hash
-    
+
     expect(coords[:results].first[:locations].first[:latLng].keys).to eq([:lat, :lng])
+  end
+
+  it "returns travel time between locations" do
+
+    origin = "Denver,CO",
+    destination = "Pueblo,CO"
+
+    service = MapquestService.new
+    time = service.get_travel_time(destination, origin)
+
+    expect(time).to be_a Hash
+    expect(time[:route]).to have_key(:formattedTime)
   end
 end
